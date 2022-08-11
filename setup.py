51ca064ca27d822b06dd11295d51a2104b1505e4
Franz Hansmair <d.hansmair@campus.lmu.de>
2022-08-11 15:41:31 +0200

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_packages
 
-# Prepend pycocoevalcap to package names
-# package_names = ['pycocoevalcap.'+p for p in find_namespace_packages()]
 
 with open("README.md", "r") as fh:
     readme = fh.read()
@@ -16,9 +14,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dhansmair/pycc3mevalcap",
     packages=find_packages(),
-    package_data={'': ['*.jar', '*.gz']},
     install_requires=[
         'pycocoevalcap'
         ],
-    python_requires='>=3'
+    python_requires='>=3.6'
 )
